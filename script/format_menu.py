ce80128f26ea3e3df6011e998639eac6574cf42f
admin <56008174@qq.com>
2021-05-17 15:47:33 +0800

    1

--- script/format_menu.py
+++ script/format_menu.py
@@ -5,7 +5,7 @@ def format_menu(choice_list) -> list:
                 'IQIYI': '爱奇艺', 'DX': 'DirectX9', '163music': '网易云音乐', 'SougouPY': '搜狗输入法', 'QQmusic': 'QQ音乐',
                 'Dtalk': '钉钉', 'Kugou': '酷狗音乐', '2345explorer': '2345浏览器', '2345pinyin': '2345拼音输入法', 'WPS': 'WPS',
                 'sys_cra': '系统优化', 'T20': '天正建筑T20', 'PSCS3': 'PhotoShop CS3', 'PSCC2018': 'PhotoShop CC2018',
-                'OFFICE2013': 'Office 2013 Professional', 'PRCC2018': 'Premiere CC2018'}
+                'OFFICE2013': 'Office 2013 Professional', 'PRCC2018': 'Premiere CC2018', 'Xunlei': '迅雷'}
 
     menu_temp = choice_list.copy()
     for item in menu_temp:
