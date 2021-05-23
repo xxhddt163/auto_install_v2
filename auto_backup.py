6446e738b126cec5f28cd6e7dd93b4e01d957160
admin <56008174@qq.com>
2021-05-24 01:20:45 +0800

    僵尸毁灭工程存档自动增量备份

--- auto_backup.py
+++ auto_backup.py
@@ -6,6 +6,7 @@ from getpass import getuser
 username = getuser()  # 当前用户名
 print(fr"当前用户名为{username},游戏存档目录为C:\Users\{username}\Zomboid\Saves")
 
+
 def check_dir():  # 目录检查
     print("正在检查存档备份目录......")
     os.chdir(fr'C:\Users\{username}\Zomboid')
@@ -33,11 +34,12 @@ def zip():
 def check_files():  # 文件检查
     print("正在检查存档文件个数......")
     os.chdir(fr'C:\Users\{username}\Zomboid\Saves_back')
-    print(f"正在备份存档，当前一共有{len(os.listdir(os.getcwd()))}个存档")
+    print(fr"C:\Users\{username}\Zomboid\Saves_back目录下一共有{len(os.listdir(os.getcwd()))}个存档")
     while True:
         if len(os.listdir(os.getcwd())) < 10:
             break
         target_file = os.listdir(os.getcwd())[0]
+        print(f"存档文件大于10个、正在删除存档文件：{target_file}")
         os.remove(os.path.join(fr'C:\Users\{username}\Zomboid\Saves_back', target_file))
 
 
